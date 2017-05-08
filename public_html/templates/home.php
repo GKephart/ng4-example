b0dfe257519368a36cf7556aef777942a705a29d
George Kephart <Gkephart@cnm.edu>
2017-05-08 10:41:31 -0600

    npm run build was successful on my local machine now time to test it on the server

---
+++ public_html/templates/home.php
@@ -0,0 +1,13 @@
+<main-nav></main-nav>
+
+<main class="bg-info">
+
+	<div class="container-fluid">
+		<div class="row">
+
+			<div class="container center-splash">
+				this is a template
+			</div>
+		</div>
+	</div>
+</main>
