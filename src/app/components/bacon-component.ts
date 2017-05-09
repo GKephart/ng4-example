837b52b8b9e93d03b0a776c40d31e2bc671afcfa
Dylan McDonald <dmcdonald21@cnm.edu>
2017-05-08 22:52:34 -0600

    autoloaded bacon goodness

--- src/app/components/bacon-component.ts
+++ src/app/components/bacon-component.ts
@@ -1,7 +1,24 @@
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import {BaconService} from "../services/bacon-service";
 
 @Component({
 	templateUrl: "./templates/bacon.php"
 })
 
-export class BaconComponent {}
+export class BaconComponent implements OnInit {
+
+	numParagraphs : number = 3;
+	paragraphs : string[] = [];
+
+	constructor(private baconService: BaconService) {}
+
+	ngOnInit() : void {
+		this.getBacon();
+	}
+
+	getBacon() : void {
+		this.baconService.getBacon(this.numParagraphs)
+			.subscribe(paragraphs => this.paragraphs = paragraphs);
+	}
+
+}
